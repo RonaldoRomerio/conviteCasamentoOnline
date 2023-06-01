97bf4dff3fe792f980e31fba99b6f6e33528ff37
Ronaldo Romério <ronaldo.romeriosj@gmail.com>
2023-06-01 00:18:23 -0300

    Alterações na mensagem ao convidado
    Separando divs
    adicionando icons

--- client/src/Pages/Convite/index.js
+++ client/src/Pages/Convite/index.js
@@ -1,39 +1,53 @@
 import React from 'react';
 import './style.css'
+import { BsCheckAll, BsBox2Heart, BsPinMap } from "react-icons/bs"
 export default function convite() {
     return (
         <div id="telaConvite">
             <main id="mensagemConvidado">
-                <p className='LetraEscrita'>Samantha e Ronaldo</p>
-                <p className='textoGeral'>Convidam</p>
+                <p className='LetraEscrita'> Samantha e Ronaldo</p>
+                <p>convidam</p>
                 <p className='LetraEscrita'>Samuel e Juliana</p> 
-                <p className='textoGeral'>para celebrar conosco e compartilhar dessa grande alegria.</p>
-                <p className='textoGeral'>Sua presença é muito importante para nós.</p>
+                <p>para celebrar conosco e compartilhar dessa grande alegria.</p>
+                <p>Sua presença é muito importante para nós.</p>
             </main>
-            <div id="dataEvento">
-                <p className='textoGeral'>Sábado</p>
+                <div id="dataEvento">
+                <p>Sábado</p>
                 <p className='LetraEscrita'>20</p>
-                <p className='textoGeral'>Janeiro</p>
-                <p className='textoGeral'>19:30</p>
+                <p>Janeiro</p>
+                <p>19:30</p>
             </div>
             <div id="botoesAcao">
                 <div className='botao'>
-                    <button>Confirmação</button>
+                    <button><BsCheckAll size={25}/></button>
                     <p className='textoBotao'>Confirmação</p>
                 </div>
                 <div className='botao'>
-                    <button>Local</button>
+                    <button><BsPinMap size={25}/></button>
                     <p className='textoBotao'>Local</p>
                 </div>
                 <div className='botao'>
-                    <button>Lista de presentes</button>
+                    <button><BsBox2Heart size={25}/></button>
                     <p className='textoBotao'>Lista de presentes</p>
                 </div>
-                
+                <div className='linha'/>
+                <footer>
+                    <p className='LetraEscrita'>Contamos com a sua presença para brindar conosco</p>
+                </footer>
             </div>
-            <footer>
-                <p className='LetraEscrita'>Contamos com a sua presença para brindar conosco</p>
-            </footer>
         </div>
     );
 }
+
+
+
+
+/*
+confirmação: BsCheckAll
+presentes: BsBox2Heart
+localização: BsPinMap
+
+
+
+
+*/
